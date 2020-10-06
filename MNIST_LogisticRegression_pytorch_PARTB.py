a71d0dede31b6682b051c0fb241bb176247a1b4c
Praveena Kondepudi <praveenakondepudi@Praveenas-MacBook-Pro.local>
2020-10-06 16:38:19 -0400

    commit

--- MNIST_LogisticRegression_pytorch_PARTB.py
+++ MNIST_LogisticRegression_pytorch_PARTB.py
@@ -6,15 +6,9 @@ import torch.optim as optim
 import torchvision.transforms as transforms
 # Plotting libraries
 import matplotlib.pyplot as plt
-import numpy as np # using only plotting in imshow method
+import numpy as np
 
-# General utlity method
-def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
-    
+
 # 1.Loading and Normalizing MNIST dataset
 transform = transforms.Compose(
     [transforms.ToTensor(),
